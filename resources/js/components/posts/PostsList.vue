ae1905adc3dd9c30a728f79a2d78ef5ffb5e8294
torone89 <torone89>
2022-10-04 16:27:10 +0200

    ok

---
+++ resources/js/components/posts/PostsList.vue
@@ -0,0 +1,65 @@
+<template>
+  <section id="posts-list">
+    <h2 class="my-3">Posts</h2>
+
+    <!-- AppLoader -->
+    <app-loader v-if="isLoading" />
+
+    <!-- Error -->
+
+    <div
+      class="alert alert-danger alert-dismissible fade show"
+      role="alert"
+      v-else-if="error"
+    >
+      <p>{{ error }}</p>
+      <button type="button" class="close" @click="error = null">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+
+    <div v-else>
+      <div v-if="posts.length">
+        <post-card v-for="post in posts" :key="post.id" :post="post" />
+      </div>
+      <h5 v-else>Non ci sono posts</h5>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLoader from "../AppLoader.vue";
+import PostCard from "./PostCard.vue";
+export default {
+  name: "PostsList",
+  data() {
+    return {
+      posts: [],
+      isLoading: false,
+      error: null,
+    };
+  },
+  components: { PostCard, AppLoader },
+  methods: {
+    fetchPosts() {
+      this.isLoading = true;
+      axios
+        .get("http://localhost:8000/api/posts")
+        .then((res) => {
+          this.posts = res.data;
+        })
+        .catch((err) => {
+          this.error = "Errore durante il fetch dei post";
+        })
+        .then(() => {
+          this.isLoading = false;
+        });
+    },
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+};
+</script>
+<style scoped lang="scss">
+</style>
